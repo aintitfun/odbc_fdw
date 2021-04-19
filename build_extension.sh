f42670179b48be56ce3431256df7082def914210
aintitfun <opolofdez@gmail.com>
2021-04-19 14:06:10 +0200

    some other changes

--- build_extension.sh
+++ build_extension.sh
@@ -1,4 +1,11 @@
-export PATH=$PATH:`echo -n $HOME`/build_extensions/postgres_binaries/`echo -n $1`/bin
-cp -R echo -n $1`_related/* .
-make
-git checkout HEAD~1 -- . 
+cp -R ${1}_related/* .
+make && make install
+mkdir -p temp_zip/bin/
+mkdir -p temp_zip/lib/
+mkdir -p temp_zip/share/extension/
+cp ${HOME}/build_extensions/postgres_binaries/${1}/bin/${PWD##*/}* temp_zip/bin
+cp ${HOME}/build_extensions/postgres_binaries/${1}/lib/${PWD##*/}* temp_zip/lib
+cp ${HOME}/build_extensions/postgres_binaries/${1}/share/extension/${PWD##*/}* temp_zip/share/extension
+
+zip ${PWD##*/}.zip temp_zip/*
+git checkout HEAD~1 -- .
